478fb581fc0d19c0efe6a39e3d21a3814d31f1c9
Clay Stewart <cstewart@rosettastone.com>
2014-11-19 00:21:20 -0600

    Add angular-timer bower * switch hg-app name to myTimer to avoid naming conflicts with angular-timer

--- public/angular/main.js
+++ public/angular/main.js
@@ -2,7 +2,7 @@
 
 var timer = (function(angular) {
 
-  var _app = angular.module('timer', ['ui.bootstrap']);
+  var _app = angular.module('myTimer', ['ui.bootstrap', 'timer']);
 
   return {
     app: _app
