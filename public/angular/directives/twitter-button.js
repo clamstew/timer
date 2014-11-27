829354e73febe532252c48c171537db3b38eb749
Clay Stewart <cstewart@rosettastone.com>
2014-11-27 16:47:09 -0600

    Add more spaces to twitter js code.

--- public/angular/directives/twitter-button.js
+++ public/angular/directives/twitter-button.js
@@ -4,12 +4,12 @@
 
   app.directive('twitterBtn', [function() {
     var link = function() {
-      !function(d,s,id){
-        var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+      !function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location)?'http':'https';
         if(!d.getElementById(id)){
-          js=d.createElement(s);
-          js.id=id;
-          js.src=p+'://platform.twitter.com/widgets.js';
+          js = d.createElement(s);
+          js.id = id;
+          js.src = p+'://platform.twitter.com/widgets.js';
           fjs.parentNode.insertBefore(js,fjs);
         }
       }(document, 'script', 'twitter-wjs');
